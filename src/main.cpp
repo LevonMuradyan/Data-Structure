3054a5d6fe56ea727e85e9ec560a84f8fbced0b7
LevonMuradyan <levon.muradyan9559@gmail.com>
2020-01-27 02:23:57 +0400

    Finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,66 @@
 #include <iostream>
 #include "vector.hpp"
-using namespace DS;
-using namespace std;
-
-void print(const vector<int>& vec) {
-
-
-    cout << "size = " << vec.size() << endl;
-    cout << "capacity = " << vec.capacity() << endl;
-    cout << "elements = " << " ";
-    for (int i = 0; i < vec.size(); ++i) {
-        cout << vec[i] << " ";
-
-    }
-    cout << endl << endl;
-}
-
+#include "test.hpp"
 
 int main() {
-
-    vector<int> q((1, 2, 3), 3);
-    cout << "q:" << endl;
-    print(q);
-
-    vector<int> a;
-    cout << "a:" << endl;
-    print(a);
-
-    vector<int> b(0);
-    cout << "b:" << endl;
-    print(b);
-
-    vector<int> c(10);
-    cout << "c:" << endl;
-    print(c);
-
-    vector<int> d(10, 0);
-    cout << "d:" << endl;
-    print(d);
-
-    vector<int> e(5, 10);
-    cout << "e:" << endl;
-    print(e);
-
-    vector<int> f = { 1,2,3 };
-    cout << "f:" << endl;
+    int value = 595;
+    int index = 4;
+ 
+    std::cout << "After vector<int> f = {1,2,3,4,5,6,7,8,9,10} :" << std::endl;
+    DS::vector<int> f = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     print(f);
+    TEST(f, value, index);
 
-    vector<int> g = { 0 };
-    cout << "g:" << endl;
-    print(g);
+    /*std::cout << "After vector<int> q((1, 2, 3), 3) :" << std::endl;
+    DS::vector<int> q((1, 2, 3), 3);
+    print(q);
+    TEST(q, value, index);*/
 
-    g = { 5,4,6 };
-    cout << "g:" << endl;
-    print(g);
+    /*std::cout << "After vector<int> a :" << std::endl;
+    DS::vector<int> a;
+    print(a);
+    TEST(a, value, index);*/
 
-    a = b;
-    std::cout<< "a = b" << std::endl;
+    /*std::cout << "After vector<int> b(0) :" << std::endl;
+    DS::vector<int> b(0);
+    print(b);
+    TEST(b, value, index);*/
+
+    /*std::cout << "After vector<int> c(10) :" << std::endl;
+    DS::vector<int> c(10);
+    print(c);
+    TEST(c, value, index);*/
+
+    /*std::cout << "After vector<int> d(10, 0) :" << std::endl;
+    DS::vector<int> d(10, 0);
+    print(d);
+    TEST(d, value, index);*/
+
+    /*std::cout << "After vector<int> e(5, 10) :" << std::endl;
+    DS::vector<int> e(5, 10);
+    print(e);
+    TEST(e, value, index);*/
+
    
+    /*std::cout << "After vector<int> g = {0} :" << std::endl;
+    DS::vector<int> g = { 0 };
+    print(g);
+    TEST(g, value, index);*/
+
+    /*std::cout << "After g = {5, 4, 6} :" << std::endl;
+    g = { 5,4,6 };
+    print(g);*/
+
+    /*std::cout<< "After a = b" << std::endl;
+    a = b;
+    print(a);*/
+
+    /*std::cout << "After vector<int> k(ptr,10) :" << std::endl;
+    int *ptr = new int[5];
+    DS::vector<int> k(ptr,10);
+    print(k);
+    TEST(k, value, index);*/
+    
     return 0;
 }
 
